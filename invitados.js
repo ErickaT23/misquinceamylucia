9a93455fb611f7e893184380c192fed6bd30f3f0
Ericka Tuchan <125705781+ErickaT23@users.noreply.github.com>
2025-10-11 16:30:14 -0600

    invitados.js

--- invitados.js
+++ invitados.js
@@ -1,3 +1,23 @@
 window.invitados = {
-  '1': { nombre: 'Isaías Cruz y Fam', adultos: 2, ninos: 2 }
+  '1': { nombre: 'Isaías Cruz y Fam.', adultos: 2, ninos: 2 },
+  '2': { nombre: 'Juan Jose Velasquez y Fam', adultos: 2, ninos: 2 },
+  '3': { nombre: 'Milton Reyes y Fam', adultos: 2, ninos: 0 },
+  '4': { nombre: 'Rolando Benavente y Fam', adultos: 5, ninos: 0 },
+  '5': { nombre: 'Jorge Morates', adultos: 1, ninos: 0 },
+  '6': { nombre: 'Aníbal Viruela y Fam', adultos: 2, ninos: 2 },
+  '7': { nombre: 'Sandra Morales', adultos: 2, ninos: 1 },
+  '8': { nombre: 'Elisa Sian', adultos: 1, ninos: 0 },
+  '9': { nombre: 'Cindy Hernandez', adultos: 2, ninos: 1 },
+  '10': { nombre: 'Dimas Pleitez y Fam', adultos: 3, ninos: 2 },
+  '11': { nombre: 'Julio Ávila y Fam', adultos: 4, ninos: 1 },
+  '12': { nombre: 'Sandra Guerra e hija', adultos: 2, ninos: 1 },
+  '13': { nombre: 'Miguel Aceytuno y Fam', adultos: 3, ninos: 2 },
+  '14': { nombre: 'Luis Roque y Fam', adultos: 4, ninos: 0 },
+  '15': { nombre: 'Sandra Chávez y Hanna Monroy', adultos: 2, ninos: 0 },
+  '16': { nombre: 'Alejandra Leiva y Fam', adultos: 2, ninos: 1 },
+  '17': { nombre: 'Andrea Chávez', adultos: 1, ninos: 1 },
+  '18': { nombre: 'Milagro Orellana', adultos: 2, ninos: 0 },
+  '19': { nombre: 'Irma Morales', adultos: 3, ninos: 0 },
+  '20': { nombre: 'Elisa Rivas y Nancy García', adultos: 2, ninos: 1 }
 };
+
