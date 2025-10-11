ead2acf857eb00aab7de74eda0866835a1bf62fb
Ericka Tuchan <125705781+ErickaT23@users.noreply.github.com>
2025-10-11 17:56:06 -0600

    virula

--- invitados.js
+++ invitados.js
@@ -4,7 +4,7 @@ window.invitados = {
   '3': { nombre: 'Milton Reyes y Fam', adultos: 2, ninos: 0 },
   '4': { nombre: 'Rolando Benavente y Fam', adultos: 5, ninos: 0 },
   '5': { nombre: 'Jorge Morales', adultos: 1, ninos: 0 },
-  '6': { nombre: 'Aníbal Viruela y Fam', adultos: 2, ninos: 2 },
+  '6': { nombre: 'Aníbal Virula y Fam', adultos: 2, ninos: 2 },
   '7': { nombre: 'Sandra Morales', adultos: 2, ninos: 1 },
   '8': { nombre: 'Elisa Sian', adultos: 1, ninos: 0 },
   '9': { nombre: 'Cindy Hernandez', adultos: 2, ninos: 1 },
