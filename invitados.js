696851aaf5086458131651a41f3a72098da71343
Ericka Tuchan <125705781+ErickaT23@users.noreply.github.com>
2025-10-10 20:09:43 -0600

    invitados

--- invitados.js
+++ invitados.js
@@ -1,5 +1,5 @@
 window.invitados = {
-  '1': { nombre: 'Juan & Marcia', pases: 2 },
+  '1': { nombre: 'Isaías Cruz y Fam', pases: 4 },
   '2': { nombre: 'Ana Pérez', pases: 3 },
   '3': { nombre: 'Luis García', pases: 4 }
 };
